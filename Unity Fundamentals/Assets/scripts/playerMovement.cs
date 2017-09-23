6b22749665d7e555cda5e9235c49728353306dee
William <messhias@gmail.com>
2017-09-23 01:59:16 -0300

    rotate tutorial

---
+++ Unity Fundamentals/Assets/scripts/playerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public float speed = 5.0f;
+    public float rotateSpeed = 100.0f;
+    	
+	// Update is called once per frame
+	void Update () {
+
+        Movement();
+        
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(new Vector3(0, -rotateSpeed * Time.deltaTime, 0));
+            this.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+            this.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+
+    }
+}
